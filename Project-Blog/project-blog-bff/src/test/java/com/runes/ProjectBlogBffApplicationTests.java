8e0c212a4457cd999b23d98317b7bd4961dffa29
runes <1634807103@qq.com>
2023-12-23 03:12:48 +0800

    init bff env

---
+++ Project-Blog/project-blog-bff/src/test/java/com/runes/ProjectBlogBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlogBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
